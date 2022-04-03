5f51884bbd408508fe37343b83f4df7c37ea5ca7
Andrew <andrew@pureaquasprings.com>
2022-04-03 17:37:53 +1000

    completed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-function fetchBooks() {
+/*function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(Response => Response.json())
+  .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
@@ -9,9 +11,29 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
-  });
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
-});
+});*/
+
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(Response => Response.json())
+  .then(json => renderBooks(json))
+}
+ 
+function renderBooks(books) {
+  const main = document.querySelector('main');
+  books.forEach(book => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+    
+  })
+    
+}
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+})
